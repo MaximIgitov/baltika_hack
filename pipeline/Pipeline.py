3f6791c51dfa4fdcfcd71bac33dc0632f87d0893
Guettoman <van_vin04@mail.ru>
2025-02-25 03:09:03 +0300

    Updated pipeline

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -7,14 +7,20 @@ import optuna
 from optuna.samplers import TPESampler
 import lightgbm as lgb
 
+from openfe import OpenFE, transform
+
 # Датасет, хранящий ошибки
 df = pd.read_excel("Categorical breakdowns.xlsx")
 
 # Датасеты признаков и таргетов, генерируемые из датасета ошибок
-features_data = pd.read_excel("Features_3H.xlsx")
+feature_data = pd.read_excel("Features_3H.xlsx")
 target_data = pd.read_excel("Target_3H.xlsx")
 
 WINDOW_SIZES = [1, 3, 7, 14, 30, 60, 180]
+PREDICTION_HORIZON = 3
+
+model1 = 0
+model2 = 0
 
 def insert_breakdown(reason: str, start: pd.Timestamp, end: pd.Timestamp, duration: float) -> None:
     # Генерация короткого имени на основе уже размеченных данных
@@ -23,7 +29,8 @@ def insert_breakdown(reason: str, start: pd.Timestamp, end: pd.Timestamp, durati
     df.drop_duplicates()
 
 def generate_features_and_targets(start: pd.Timestamp, end: pd.Timestamp, freq: int = 3, prediction_horizon: int = 3) -> None:
-    global feature_data, target_data, df
+    global feature_data, target_data, df, PREDICTION_HORIZON
+    PREDICTION_HORIZON = prediction_horizon
     freq_str = f'{freq}h'
     time_range = pd.date_range(start=start, end=end, freq='3h')
     
@@ -70,5 +77,158 @@ def generate_features_and_targets(start: pd.Timestamp, end: pd.Timestamp, freq: 
     feature_df = pd.DataFrame(feature_rows)
     target_df = pd.DataFrame(target_rows)
 
-def fit_model() -> None:
+
+def fit_model(reason: str, top_breakdowns_num: int = 4) -> None:
+    global feature_data, target_data, model1, model2
+    top_breakdowns = target_data["next_breakdown"].value_counts().nlargest(top_breakdowns_num).index
+    target_data_red = target_data[target_data["next_breakdown"].isin(top_breakdowns)]
+
+    feature_data_red = feature_data.loc[feature_data.index.isin(target_data_red.index)]
+    
+    # Загрузка данных
+    X = feature_data_red.drop(columns=["date"])
+
+    # Преобразуем события в числовые метки
+    event_encoder = LabelEncoder()
+    y = event_encoder.fit_transform(target_data_red["next_breakdown"])
+
+    # Разделение данных на обучающую и тестовую выборки
+    train_size = int(0.8 * len(X))
+    X_train, X_test, y_train, y_test = X[:train_size], X[train_size:], y[:train_size], y[train_size:]
+    
+    # Преобразование y в бинарный формат
+    lb = LabelBinarizer()
+    y_test_binarized = lb.transform(y_test)
+
+    # Функция для Optuna
+    def objective(trial):
+        n_estimators = trial.suggest_int('n_estimators', 50, 200)
+        max_depth = trial.suggest_int('max_depth', 10, 50)
+        learning_rate = trial.suggest_float('learning_rate', 0.01, 0.1)
+
+        model = lgb.LGBMClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, random_state=239, verbose=-1)
+    
+        model.fit(X_train, y_train)
+        y_pred_proba = model.predict_proba(X_test)[:]
+        return roc_auc_score(y_test_binarized, y_pred_proba, multi_class='ovr')
+
+    study = optuna.create_study(direction='maximize', sampler=TPESampler())
+    study.optimize(objective, n_trials=50)
+
+    # Лучшие параметры
+    best_params = study.best_params
+    print(f'Best ROC-AUC: {study.best_value:.2f}')
+    print(f'Best parameters: {best_params}')
+
+    # Обучение модели с лучшими параметрами
+    model1 = lgb.LGBMClassifier(**best_params, random_state=42)
+
+    model1.fit(X_train, y_train)
+    
+    feature_importances = model1.feature_importances_
+
+    feature_importance_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': feature_importances})
+    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+
+    # Отберем 50 самых важных признаков
+    feature_importance_df_selected = feature_importance_df[:50]
+    
+    X = feature_data_red.drop(columns=["date"])[feature_importance_df_selected["Feature"]]
+
+    X_train, X_test, y_train, y_test = X[:train_size], X[train_size:], y[:train_size], y[train_size:]
+    
+    #Генерируем новые признаки
+    ofe = OpenFE()
+    features = ofe.fit(data=X_train, label=y_train)
+    train_x, test_x = transform(X_train, X_test, features, n_jobs=8)
+
+    # Функция для Optuna
+    def objective(trial):
+        n_estimators = trial.suggest_int('n_estimators', 50, 200)
+        max_depth = trial.suggest_int('max_depth', 5, 50)
+        learning_rate = trial.suggest_float('learning_rate', 0.01, 0.1)
+
+        model = lgb.LGBMClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, random_state=239, verbose=-1)
+    
+        model.fit(train_x, y_train)
+        y_pred_proba = model.predict_proba(test_x)[:]
+        return roc_auc_score(y_test_binarized, y_pred_proba, multi_class='ovr')
+
+    study = optuna.create_study(direction='maximize', sampler=TPESampler())
+    study.optimize(objective, n_trials=15)
+
+    # Обучение модели с лучшими параметрами
+    model1 = lgb.LGBMClassifier(**best_params, random_state=42)
+
+    model1.fit(train_x, y_train)
+
+    
+    # Загрузка данных
+    X = feature_data.drop(columns=["date"])
+
+    # Преобразуем события в числовые метки
+    event_encoder = LabelEncoder()
+    y = event_encoder.fit_transform(target_data[f'{reason}_within_next_{PREDICTION_HORIZON}_days'])
+    
+    train_size = int(0.8 * len(X))
+    X_train, X_test, y_train, y_test = X[:train_size], X[train_size], y[:train_size], y[train_size:]
+    
+    from sklearn.metrics import f1_score
+
+    def objective(trial):
+        n_estimators = trial.suggest_int('n_estimators', 50, 200)
+        max_depth = trial.suggest_int('max_depth', 10, 50)
+        learning_rate = trial.suggest_float('learning_rate', 0.01, 0.1)
+
+        model = lgb.LGBMClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, random_state=239, verbose=-1)
+    
+        model.fit(X_train, y_train)
+    
+        y_pred = model.predict(X_test)
+    
+        return f1_score(y_test, y_pred, average='weighted')
+
+    study = optuna.create_study(direction='maximize', sampler=TPESampler())
+    study.optimize(objective, n_trials=50)
+
+    model2 = lgb.LGBMClassifier(**best_params, random_state=42)
+
+    model2.fit(X_train, y_train)
+    
+    feature_importances = model2.feature_importances_
+
+    feature_importance_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': feature_importances})
+    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+
+    # Отберем 50 самых важных признаков
+    feature_importance_df_selected = feature_importance_df[:50]
+    
+    X = feature_data.drop(columns=["date"])[feature_importance_df_selected["Feature"]]
+
+    X_train, X_test, y_train, y_test = X[:train_size], X[train_size:], y[:train_size], y[train_size:]
+    
+    #Генерируем новые признаки
+    ofe = OpenFE()
+    features = ofe.fit(data=X_train, label=y_train)
+    train_x, test_x = transform(X_train, X_test, features, n_jobs=8)
+
+    # Функция для Optuna
+    def objective(trial):
+        n_estimators = trial.suggest_int('n_estimators', 50, 200)
+        max_depth = trial.suggest_int('max_depth', 5, 50)
+        learning_rate = trial.suggest_float('learning_rate', 0.01, 0.1)
+
+        model = lgb.LGBMClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, random_state=239, verbose=-1)
+    
+        model.fit(train_x, y_train)
+        y_pred_proba = model.predict_proba(test_x)[:]
+        return roc_auc_score(y_test_binarized, y_pred_proba, multi_class='ovr')
+
+    study = optuna.create_study(direction='maximize', sampler=TPESampler())
+    study.optimize(objective, n_trials=15)
+
+    # Обучение модели с лучшими параметрами
+    model2 = lgb.LGBMClassifier(**best_params, random_state=42)
+
+    model2.fit(train_x, y_train)
     
